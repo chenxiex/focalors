b6c0f24fb842f74bb07ea7a30e0afaff48a2702c
Anlor <chenxiex@outlook.com>
2025-02-10 11:20:42 +0000

    更新focalors.h中的函数文档注释，添加@brief描述

--- include/focalors.h
+++ include/focalors.h
@@ -12,8 +12,7 @@ namespace focalors
 // DES
 
 /*
- * DES加密或解密。
- *
+ * @brief DES加密或解密。
  * @param input 输入数据。
  * @param key 密钥。
  * @param encrypt true表示加密，false表示解密。
@@ -24,8 +23,7 @@ std::vector<uint8_t> des(const std::vector<uint8_t> &input, const std::vector<ui
 // AES
 
 /*
- * AES加密或解密。
- *
+ * @brief AES加密或解密。
  * @param input 输入数据。
  * @param key 密钥。
  * @param encrypt true表示加密，false表示解密。
@@ -81,43 +79,37 @@ void elgamal_decrypt(std::string &m, const std::string &c1, const std::string &c
 // utils
 
 /*
- * 将二进制字符串转换为字节序列。字符串不能被8整除的部分将被忽略。
- *
+ * @brief 将二进制字符串转换为字节序列。
  * @param binary 二进制字符串。
  * @return 字节序列。
  */
 std::vector<uint8_t> binary_to_bytes(const std::string &binary);
 /*
- * 将字节序列转换为二进制字符串。
- *
+ * @brief 将字节序列转换为二进制字符串。
  * @param bytes 字节序列。
  * @return 二进制字符串。
  */
 std::string bytes_to_binary(const std::vector<uint8_t> &bytes);
 /*
- * 将二进制字符串转换为十六进制字符串。
- *
+ * @brief 将二进制字符串转换为十六进制字符串。
  * @param binary 二进制字符串。
  * @return 十六进制字符串。
  */
 std::string binary_to_hex(const std::string &binary);
 /*
- * 将十六进制字符串转换为二进制字符串。
- *
+ * @brief 将十六进制字符串转换为二进制字符串。
  * @param hex 十六进制字符串。
  * @return 二进制字符串。
  */
 std::string hex_to_binary(const std::string &hex);
 /*
- * 将十六进制字符串转换为字节序列。
- *
+ * @brief 将十六进制字符串转换为字节序列。
  * @param hex 十六进制字符串。
  * @return 字节序列。
  */
 std::vector<uint8_t> hex_to_bytes(const std::string &hex);
 /*
- * 将字节序列转换为十六进制字符串。
- *
+ * @brief 将字节序列转换为十六进制字符串。
  * @param bytes 字节序列。
  * @return 十六进制字符串。
  */
