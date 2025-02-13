91dbe74e1ad3668dd26bc313b04cb5aa5dba279e
Anlor <chenxiex@outlook.com>
2025-02-13 04:30:47 +0000

    新增调试模块，包含CMake配置和主程序文件

---
+++ debug/debug.cpp
@@ -0,0 +1,9 @@
+#include "debug.h"
+#include "../tests/include/test.h"
+#include "focalors.h"
+using namespace focalors;
+using namespace std;
+
+int main()
+{
+}
---
+++ debug/debug.h
@@ -0,0 +1,1 @@
+#include "focalors.h"
