3ce83442ee240ec942344160b1befae52fb24077
Anlor <chenxiex@outlook.com>
2025-03-11 03:22:23 +0000

    为simple_block_cipher类添加头文件保护

--- tests/include/test.h
+++ tests/include/test.h
@@ -1,3 +1,6 @@
+#pragma once
+#ifndef TEST_H
+#define TEST_H
 #include "focalors.h"
 #include <cstdint>
 #include <vector>
@@ -11,6 +14,9 @@ class simple_block_cipher : public focalors::block_cipher
 {
   public:
     size_t block_size() const noexcept override;
-    std::vector<uint8_t> encrypt(std::vector<uint8_t>::const_iterator first, std::vector<uint8_t>::const_iterator last, const std::vector<uint8_t> &key) const override;
-    std::vector<uint8_t> decrypt(std::vector<uint8_t>::const_iterator first, std::vector<uint8_t>::const_iterator last, const std::vector<uint8_t> &key) const override;
+    std::vector<uint8_t> encrypt(std::vector<uint8_t>::const_iterator first, std::vector<uint8_t>::const_iterator last,
+                                 const std::vector<uint8_t> &key) const override;
+    std::vector<uint8_t> decrypt(std::vector<uint8_t>::const_iterator first, std::vector<uint8_t>::const_iterator last,
+                                 const std::vector<uint8_t> &key) const override;
 };
+#endif
