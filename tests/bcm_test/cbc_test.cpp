c506894a7ee85633324622db45f90e81a71eaf4c
Anlor <chenxiex@outlook.com>
2025-03-11 03:53:59 +0000

    使用aes加密来进行测试。使用openssl验证测试用例。

--- tests/bcm_test/cbc_test.cpp
+++ tests/bcm_test/cbc_test.cpp
@@ -1,3 +1,4 @@
+// echo -n "input_hex" | xxd -r -p | openssl enc -aes-128-cbc -K "key_hex" -nosalt -nopad -iv "z_hex" | xxd -p
 #include "focalors.h"
 #include "test.h"
 #include "gtest/gtest.h"
@@ -6,9 +7,9 @@ using namespace std;
 
 // clang-format off
 const vector<test_case> test_cases = {
-{hex_to_bytes("0000000000000000""1111111111111111""2222222222222222"),
-hex_to_bytes("ffffffffffffffff"),
-hex_to_bytes("ffffffffffffffff""1111111111111111""cccccccccccccccc")}
+{hex_to_bytes("0001000101a198afda78173486153566""0001000101a198afda78173486153566""0001000101a198afda78173486153566"),
+hex_to_bytes("00012001710198aeda79171460153594"),
+hex_to_bytes("6cdd596b8f5642cbd23b47981a65422a5d5cd790cb302872881f96da90378f0d347096b0182788d5377f7cbe54a4f9d4")}
 };
 // clang-format on
 
@@ -19,8 +20,8 @@ TEST(BlockCipherModeTest, CBC)
         auto &plaintext = i.plaintext;
         auto &key = i.key;
         auto &ciphertext = i.ciphertext;
-        vector<uint8_t> z(simple_block_cipher().block_size(), 0);
-        auto cbc = CBC(key, simple_block_cipher(), z);
+        vector<uint8_t> z(AES().block_size(), 0);
+        auto cbc = CBC(key, AES(), z);
         // encrypt
         auto encrypted = cbc.encrypt(plaintext.begin(), plaintext.end());
         EXPECT_STREQ(bytes_to_hex(encrypted).c_str(), bytes_to_hex(ciphertext).c_str());
--- tests/bcm_test/ecb_test.cpp
+++ tests/bcm_test/ecb_test.cpp
@@ -1,3 +1,4 @@
+//echo -n "input_hex" | xxd -r -p | openssl enc -aes-128-ecb -K "key_hex" -nosalt -nopad | xxd -p
 #include "focalors.h"
 #include "test.h"
 #include "gtest/gtest.h"
@@ -6,9 +7,9 @@ using namespace std;
 
 // clang-format off
 const vector<test_case> test_cases = {
-{hex_to_bytes("0000000000000000""1111111111111111""2222222222222222"),
-hex_to_bytes("ffffffffffffffff"),
-hex_to_bytes("ffffffffffffffff""eeeeeeeeeeeeeeee""dddddddddddddddd")}
+{{hex_to_bytes("0001000101a198afda78173486153566""0001000101a198afda78173486153566""0001000101a198afda78173486153566"),
+    hex_to_bytes("00012001710198aeda79171460153594"),
+    hex_to_bytes("6cdd596b8f5642cbd23b47981a65422a""6cdd596b8f5642cbd23b47981a65422a""6cdd596b8f5642cbd23b47981a65422a")}}
 };
 // clang-format on
 
@@ -19,7 +20,7 @@ TEST(BlockCipherModeTest, ECB)
         auto &plaintext = i.plaintext;
         auto &key = i.key;
         auto &ciphertext = i.ciphertext;
-        auto ecb = ECB(key, simple_block_cipher());
+        auto ecb = ECB(key, AES());
         // encrypt
         auto encrypted = ecb.encrypt(plaintext.begin(), plaintext.end());
         EXPECT_STREQ(bytes_to_hex(encrypted).c_str(), bytes_to_hex(ciphertext).c_str());
