8808c81e60bf509e0b0748c38ad3ea2d601b43a5
Anlor <chenxiex@outlook.com>
2025-03-12 12:13:02 +0000

    移除不必要的编译参数；移除test_lib

--- tests/include/test.h
+++ tests/include/test.h
@@ -11,13 +11,4 @@ struct test_case
     std::vector<uint8_t> ciphertext;
     std::vector<uint8_t> iv;
 };
-class simple_block_cipher : public focalors::block_cipher
-{
-  public:
-    size_t block_size() const noexcept override;
-    std::vector<uint8_t> encrypt(std::vector<uint8_t>::const_iterator first, std::vector<uint8_t>::const_iterator last,
-                                 const std::vector<uint8_t> &key) const override;
-    std::vector<uint8_t> decrypt(std::vector<uint8_t>::const_iterator first, std::vector<uint8_t>::const_iterator last,
-                                 const std::vector<uint8_t> &key) const override;
-};
 #endif
